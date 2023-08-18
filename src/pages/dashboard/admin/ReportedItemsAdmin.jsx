bd220c1a7742ced7b8429ce1f6f7af0a27086053
Tofayel Hossain <78659570+Tofayel-stack@users.noreply.github.com>
2023-08-19 02:28:37 +0600

    report item add in admin route

--- src/pages/dashboard/admin/ReportedItemsAdmin.jsx
+++ src/pages/dashboard/admin/ReportedItemsAdmin.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { MdOutlineRestore } from 'react-icons/md';
 
 const ReportedItemsAdmin = () => {
 
-        // get all seller data from backend 
+        // get all reported product data from backend 
 
-        const url = 'http://localhost:5000/typeOfUser?type=Seller'
-
-        const { data=[],isLoading ,refetch}=useQuery({
-            queryKey:[url],
-            queryFn: async() => {
-                const res = await fetch(url)
-                const data = res.json()
-                return data
-            }    
+        const key = 'reportedItem'
+        const value = true
+        const {data:reportedItems=[] , refetch}=useQuery({
+          queryKey:[key,value],
+          queryFn:async ()=>{
+            const res = await fetch(`http://localhost:5000/product?key=${key}&value=${value}`)
+            const data = res.json();
+            return data;
+          }
         })
-        const allSeller = data?.data;
-    
-        console.log(allSeller);
-    
+  
+        const reportedItemsData = reportedItems.data;
 
 
     return (
                <div>
-             <h1 className="p-8 text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">All of my <span className="text-amber-500">Sellers</span></h1>
+             <h1 className="p-8 text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">All Reported<span className="text-amber-500" > Products</span></h1>
             {/* table  */}
             <div>
                 <div className="overflow-x-auto">
@@ -32,8 +33,8 @@ const ReportedItemsAdmin = () => {
                     <tr className='text-amber-600 font-semibold'>
                         
                         <th>Name</th>
-                        <th>Email</th>
-                        <th>Type</th>
+                        <th>Product Id</th>
+                        <th>Condition</th>
                         <th>Action</th>
                     </tr>
                     </thead>
@@ -44,39 +45,35 @@ const ReportedItemsAdmin = () => {
 
                     {
 
-                            allSeller && 
+                        reportedItemsData && 
                                 <>
                                 {
-                                    allSeller?.map(seller => 
-                                        <tr key={seller._id}>
+                                    reportedItemsData?.map(product => 
+                                        <tr key={product._id}>
                                     
                                         <td>
                                         <div className="flex items-center space-x-3">
                                             <div className="avatar">
                                             <div className="mask mask-squircle w-12 h-12">
-                                                <img src={seller.uploadedPhotoURL} alt="productPhoto" />
+                                                <img src={product.productPhoto} alt="productPhoto" />
                                             </div>
                                             </div>
                                             
-                                            <div className="font-bold" title="verifyed seller">{seller.userName}</div>
-                                            {
-                                                seller.verify && <MdVerifiedUser className='text-green-600'></MdVerifiedUser>
-                                            }
+                                            <div className="font-bold">{product.productName}</div>
+                                         
                                             
                                         </div>
                                         </td>
 
-                                        <td>{seller.email}</td>
+                                        <td>{product._id}</td>
 
-                                        <td className="text-amber-500">{seller.userType}</td>
+                                        <td className="text-amber-500">{product.productCondition}</td>
 
                                        
                                         <td>
-                                            <button title="Delete" onClick={()=>handleDeleteUser(seller._id)} className='btn btn-xs text-red-500 text-xl'><AiOutlineDelete/></button> 
+                                            <button title="Delete" onClick={()=>handleDeleteUser(product._id)} className='btn btn-xs text-red-500 text-xl'><AiOutlineDelete/></button> 
+                                            <button title="remove report"  className='btn btn-xs text-red-500 text-xl'><MdOutlineRestore/></button> 
 
-                                            {
-                                                !seller.verify && <button title="verify seller" onClick={()=>verifySeller(seller._id)}  className='btn btn-xs  text-blue-500'>add to verify</button>
-                                            }
                                             
                                         </td>
                                     </tr>
