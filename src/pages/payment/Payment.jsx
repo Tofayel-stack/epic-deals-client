fcc69458cc53d285a58e7c444cf74db4f950784d
Tofayel Hossain <78659570+Tofayel-stack@users.noreply.github.com>
2023-08-24 17:51:59 +0600

    payment system just lounch ..not properly done

--- src/pages/payment/Payment.jsx
+++ src/pages/payment/Payment.jsx
@@ -1,34 +1,26 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { TbCurrencyTaka } from 'react-icons/tb';
+import { Elements } from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import CheckoutForm from './CheckoutForm';
 
 const Payment = () => {
 
-    const product = useLoaderData()
-    console.log(product.data);
-    const productData = product.data 
 
-    const handlePayment = (e)=>{
-        e.preventDefault()
-        alert("payment system is coming soon ! ")
-    }
+    const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 
-
+    const options = {
+      mode: 'payment',
+      amount: 1099,
+      currency: 'usd',
+      // Fully customizable with appearance API.
+      appearance: {
+        /*...*/
+      },
+    };
     return (
-        <div className='bg-zinc-700 grid justify-center content-center h-screen' >
-            
-            <div className='bg-zinc-800 p-6 mb-3 shadow-sm  shadow-orange-600 rounded text-xl text-white text-center w-full'>This is only : {productData.price} <TbCurrencyTaka className='inline text-2xl text-amber-600'/></div>
-            
-            <form onSubmit={handlePayment}>
-               <div className='grid gap-1 grid-cols-1 bg-zinc-700'>
-                    <div className='col-span-2'><input className='w-full bg-zinc-900 p-3' type="text" placeholder='CARD NUMBER'/></div>
-                    <div className='col-span-1'><input className='w-full  bg-zinc-900 p-3' type="text" placeholder='CVC' /></div>
-                    <div className='col-span-1'><input className='w-full  bg-zinc-900 p-3' type="text" placeholder='EXPIRATION DATE'/></div>
-                    <div className='col-span-2'><input className='w-full  bg-zinc-900 p-3' type="text" placeholder='NAME OF CARD'/></div>
-             
-               </div>
-               <div><button className='btn bg-amber-600 w-full outline-none border-none text-white mt-2'>Confirm Payment</button></div>
-            </form>
+        <div>
+            <Elements stripe={stripePromise} options={options}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
