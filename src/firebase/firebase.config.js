4fdc10135d3c8b0dd654a3cc0b819072cad81529
Tofayel Hossain <78659570+Tofayel-stack@users.noreply.github.com>
2023-08-30 17:21:03 +0600

    env file problem solv

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
+// web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyD3g4pRAFr2Zp5l2LDzVz05pV228NKmEOk',
-  authDomain: 'epic-deals-ecommerce.firebaseapp.com',
-  projectId:'epic-deals-ecommerce',
-  storageBucket: 'epic-deals-ecommerce.appspot.com',
-  messagingSenderId: '690396021584',
-  appId: '1:690396021584:web:e9719a4894122813a8a255'
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
